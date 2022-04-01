a2bb6b2361a14169656c0a8c517893e60cbc522d
kmiu2 <kennymiu@rogers.com>
2022-03-31 23:06:50 -0400

    feat: socials scaffold

---
+++ lib/screens/scaffolds/socials_scaffold.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:staugustinechsnewapp/screens/main/socials_screen.dart';
+
+class SocialsScaffold extends StatefulWidget {
+  const SocialsScaffold({Key? key}) : super(key: key);
+  @override
+  State<SocialsScaffold> createState() => _SocialsScaffoldState();
+}
+
+class _SocialsScaffoldState extends State<SocialsScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(child: SocialsScreen());
+  }
+}
