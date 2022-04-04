7676de7e4149f1ae0b5cfd49736f07906d58406e
kmiu2 <kennymiu@rogers.com>
2022-04-03 22:46:22 -0400

    feat: cafe menu api

---
+++ lib/providers/cafe_menu/cafe_menu_repository.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
+import 'package:staugustinechsnewapp/models/cafe_menu/cafe_menu_item/cafe_menu_item.dart';
+import 'package:staugustinechsnewapp/models/shared/failure/failure.dart';
+import 'package:staugustinechsnewapp/providers/cafe_menu/cafe_menu_api.dart';
+
+@lazySingleton
+class CafeMenuRepository {
+  CafeMenuRepository();
+
+  static Future<Either<Failure, List<CafeMenuItem>>> getCafeMenu() async {
+    return await CafeMenuApi.getCafeMenuItems();
+  }
+}
