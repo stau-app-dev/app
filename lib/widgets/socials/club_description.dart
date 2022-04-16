02931dc9cd3dbdf165830c77309971c9905a3a93
kmiu2 <kennymiu@rogers.com>
2022-04-16 16:33:16 -0400

    feat: socials

--- lib/widgets/socials/club_description.dart
+++ lib/widgets/socials/club_description.dart
@@ -3,7 +3,7 @@ import 'package:staugustinechsnewapp/styles.dart';
 import 'package:staugustinechsnewapp/widgets/reusable/basic_container.dart';
 import 'package:staugustinechsnewapp/widgets/reusable/rounded_button.dart';
 
-class ClubDescription extends StatefulWidget {
+class ClubDescription extends StatelessWidget {
   final String description;
   final String? instructionsToJoin;
   final Function()? onPressJoin;
@@ -15,14 +15,9 @@ class ClubDescription extends StatefulWidget {
       this.onPressJoin})
       : super(key: key);
 
-  @override
-  State<ClubDescription> createState() => _ClubDescriptionState();
-}
-
-class _ClubDescriptionState extends State<ClubDescription> {
   // This is unused, but it's here for future reference.
   Widget buildInstructionsToJoin() {
-    if (widget.instructionsToJoin == null || widget.onPressJoin == null) {
+    if (instructionsToJoin == null || onPressJoin == null) {
       return Container();
     }
 
@@ -33,9 +28,9 @@ class _ClubDescriptionState extends State<ClubDescription> {
         Text('Instructions to Join',
             style: Styles.normalMainText.copyWith(fontSize: 11.0)),
         const SizedBox(height: 10.0),
-        Text(widget.instructionsToJoin ?? '', style: Styles.normalText),
+        Text(instructionsToJoin ?? '', style: Styles.normalText),
         const SizedBox(height: 10.0),
-        RoundedButton(text: 'Join!', onPressed: widget.onPressJoin!),
+        RoundedButton(text: 'Join!', onPressed: onPressJoin!),
       ],
     );
   }
@@ -48,7 +43,7 @@ class _ClubDescriptionState extends State<ClubDescription> {
       children: [
         const Text('Description', style: Styles.normalMainText),
         const SizedBox(height: 10.0),
-        Text(widget.description, style: Styles.normalText),
+        Text(description, style: Styles.normalText),
         buildInstructionsToJoin(),
       ],
     ));
