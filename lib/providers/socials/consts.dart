96540267aeb2da5aae84a62f06dc1d464bbc2738
kmiu2 <kennymiu@rogers.com>
2022-04-19 23:36:40 -0400

    feat: delete annc api

--- lib/providers/socials/consts.dart
+++ lib/providers/socials/consts.dart
@@ -7,6 +7,8 @@ String getUserClubsNotJoinedEndpoint =
 String getClubEndpoint = getCloudFunctionsDomain() + '/getClub';
 String addClubAnnouncementEndpoint =
     getCloudFunctionsDomain() + '/addClubAnnouncement';
+String deleteClubAnnouncementEndpoint =
+    getCloudFunctionsDomain() + '/deleteClubAnnouncement';
 String getClubAnnouncementsEndpoint =
     getCloudFunctionsDomain() + '/getClubAnnouncements';
 String addUserToClubEndpoint = getCloudFunctionsDomain() + '/addUserToClub';
@@ -19,6 +21,7 @@ const errorGettingUserClubs = 'Error getting user clubs';
 const errorGettingUserClubsNotJoined = 'Error getting user clubs not joined';
 const errorGettingClub = 'Error getting club';
 const errorAddingClubAnnouncement = 'Error adding club announcement';
+const errorDeletingClubAnnouncement = 'Error deleting club announcement';
 const errorGettingClubAnnouncements = 'Error getting club announcements';
 const errorAddingUserToClub = 'Error adding user to club';
 const errorAddingUserToPendingClub = 'Error adding user to pending club';
