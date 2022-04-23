c83f8478c21a4a92bea6870afdbb3450673c8267
Martin <46298951+martin226@users.noreply.github.com>
2022-04-23 17:51:44 -0400

    feat: include auth token in request headers (#39)

--- lib/providers/announcements/announcements_api.dart
+++ lib/providers/announcements/announcements_api.dart
@@ -5,13 +5,15 @@ import 'package:injectable/injectable.dart';
 import 'package:staugustinechsnewapp/models/announcements/general_announcement/announcement.dart';
 import 'package:staugustinechsnewapp/models/shared/failure/failure.dart';
 import 'package:staugustinechsnewapp/providers/announcements/consts.dart';
+import 'package:staugustinechsnewapp/providers/authclient.dart';
 
 @Injectable()
 class AnnouncementsApi {
   static Future<Either<Failure, List<Announcement>>>
       getGeneralAnnouncements() async {
     try {
-      Response res = await get(Uri.parse(getGeneralAnnouncementsEndpoint));
+      Response res =
+          await authClient.get(Uri.parse(getGeneralAnnouncementsEndpoint));
       if (res.statusCode == 200) {
         List<dynamic> data = json.decode(res.body)['data'];
         List<Announcement> announcements = [];
--- lib/providers/home/home_api.dart
+++ lib/providers/home/home_api.dart
@@ -7,12 +7,13 @@ import 'package:staugustinechsnewapp/models/home/spirit_meters/spirit_meters.dar
 import 'package:staugustinechsnewapp/models/home/verse_of_day/verse_of_day.dart';
 import 'package:staugustinechsnewapp/models/shared/failure/failure.dart';
 import 'package:staugustinechsnewapp/providers/home/consts.dart';
+import 'package:staugustinechsnewapp/providers/authclient.dart';
 
 @Injectable()
 class HomeApi {
   static Future<Either<Failure, int?>> getDayNumber() async {
     try {
-      Response res = await get(Uri.parse(getDayNumberEndpoint));
+      Response res = await authClient.get(Uri.parse(getDayNumberEndpoint));
       if (res.statusCode == 200) {
         int? dayNumber =
             DayNumber.fromJson(json.decode(res.body)['data']).dayNumber;
@@ -27,7 +28,7 @@ class HomeApi {
 
   static Future<Either<Failure, SpiritMeters>> getSpiritMeters() async {
     try {
-      Response res = await get(Uri.parse(getSpiritMetersEndpoint));
+      Response res = await authClient.get(Uri.parse(getSpiritMetersEndpoint));
       if (res.statusCode == 200) {
         SpiritMeters spiritMeters =
             SpiritMeters.fromJson(json.decode(res.body)['data']);
@@ -42,7 +43,7 @@ class HomeApi {
 
   static Future<Either<Failure, VerseOfDay>> getVerseOfDay() async {
     try {
-      Response res = await get(Uri.parse(getVerseOfDayEndpoint));
+      Response res = await authClient.get(Uri.parse(getVerseOfDayEndpoint));
       if (res.statusCode == 200) {
         VerseOfDay verseOfDay =
             VerseOfDay.fromJson(json.decode(res.body)['data']);
